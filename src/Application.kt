dedd0381a635176ae16284ed029004e6db89976d
imkiva <libkernelpanic@gmail.com>
2020-05-11 02:00:35 +0800

    /greetings: return system info

--- src/Application.kt
+++ src/Application.kt
@@ -1,18 +1,20 @@
 package com.nonstop.judger
 
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
-import io.ktor.routing.*
-import io.ktor.http.*
-import io.ktor.gson.*
-import io.ktor.features.*
-import io.ktor.client.*
-import io.ktor.client.features.auth.*
-import io.ktor.client.features.json.*
-import io.ktor.client.request.*
-import kotlinx.coroutines.*
-import io.ktor.client.features.logging.*
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.client.HttpClient
+import io.ktor.client.features.auth.Auth
+import io.ktor.client.features.json.GsonSerializer
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.logging.LogLevel
+import io.ktor.client.features.logging.Logging
+import io.ktor.features.ContentNegotiation
+import io.ktor.gson.gson
+import io.ktor.response.respond
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -34,27 +36,22 @@ fun Application.module(testing: Boolean = false) {
             level = LogLevel.HEADERS
         }
     }
-    runBlocking {
-        // Sample for making a HTTP Client request
-        /*
-        val message = client.post<JsonSampleClass> {
-            url("http://127.0.0.1:8080/path/to/endpoint")
-            contentType(ContentType.Application.Json)
-            body = JsonSampleClass(hello = "world")
-        }
-        */
-    }
 
     routing {
         get("/") {
-            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+            call.respondText("hello world!")
         }
 
-        get("/json/gson") {
-            call.respond(mapOf("hello" to "world"))
+        get("/greetings") {
+            call.respond(
+                mapOf(
+                    "server" to "DanmakuJudger",
+                    "version" to 0.1,
+                    "database-by" to "Emmmer",
+                    "server-by" to "kiva",
+                    "server-time" to System.currentTimeMillis()
+                )
+            )
         }
     }
 }
-
-data class JsonSampleClass(val hello: String)
-
