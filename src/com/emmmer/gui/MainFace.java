5047baa2c50ef960970fd3af2ab1b0e9744c7adf
Shirakami Emmmer <yjhdandan@163.com>
2020-05-12 16:26:20 +0800

    change MainFace

--- src/com/emmmer/gui/MainFace.java
+++ src/com/emmmer/gui/MainFace.java
@@ -8,20 +8,32 @@ public class MainFace {
 
         panel.setLayout(null);
 
-        JLabel userLabel = new JLabel("请判断以下弹幕是否引战");
+        JLabel questionLabel = new JLabel("请判断以下弹幕是否引战");
 
-        userLabel.setBounds(10, 0, 200, 30);
-        panel.add(userLabel);
+        questionLabel.setBounds(10, 0, 200, 30);
+        panel.add(questionLabel);
 
 
-        JButton confirmButton = new JButton("danmakuText");
-        confirmButton.setBounds(10, 80, 80, 25);
-        panel.add(confirmButton);
+        JButton confirmButton1 = new JButton("danmakuText");
+        confirmButton1.setBounds(0, 80, 100, 100);
+        panel.add(confirmButton1);
+
+        JButton confirmButton2 = new JButton("danmakuText");
+        confirmButton2.setBounds(100, 80, 100, 100);
+        panel.add(confirmButton2);
+
+        JButton confirmButton3 = new JButton("danmakuText");
+        confirmButton3.setBounds(200, 80, 100, 100);
+        panel.add(confirmButton3);
+
+        JButton confirmButton4 = new JButton("danmakuText");
+        confirmButton4.setBounds(300, 80, 100, 100);
+        panel.add(confirmButton4);
     }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Judgement");
-        frame.setSize(350, 200);
+        frame.setSize(400, 200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel panel = new JPanel();
